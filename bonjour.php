d3aae7adebcae13916670d1ead49c6979f38f24a
julienvinas <vinas.julien@gmail.com>
2020-06-05 22:18:41 +0200

    try

--- bonjour.php
+++ bonjour.php
@@ -3,6 +3,14 @@
   <title>Test PHP</title>
  </head>
  <body>
- <?php echo '<p>Bonjour le monde la famille ca  va ?</p>'; ?>
+ <?php echo '<p>Bonjour le monde la famille ca  va ?</p>'; 
+ 
+ $a = array(2, 4, 6, 8);
+ echo "sum(a) = " . array_sum($a) . "\n";
+ 
+ $b = array("a" => 1.2, "b" => 2.3, "c" => 3.4);
+ echo "sum(b) = " . array_sum($b) . "\n";
+ 
+ ?>
  </body>
 </html>

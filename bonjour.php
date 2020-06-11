7747f4ed9be548237b5179f6865cc54e6cddfc49
julienvinas <vinas.julien@gmail.com>
2020-06-11 22:38:47 +0200

    1

--- bonjour.php
+++ bonjour.php
@@ -1,8 +1,21 @@
 <?php
-namespace fatboar;
-class Math{
-    public static function double($nombre){
-        return $nombre * 2;
+
+class Bonjour{
+    private $bonjour;
+
+    //get the amount of bonjour still in the machine
+    public function getBonjour(){
+        return $this->bonjour;
+    }
+
+    //set the amount of bonjour in the machine
+    public function setBonjour($amount){
+        $this->bonjour = $amount;
+    }
+
+    // the user turns the wheel, machine dispenses bonjour!
+    public function turnWheel(){
+        $this->setBonjour($this->getBonjour() -1);
     }
 
 }
--- tests/BonjourTest.php
+++ tests/BonjourTest.php
@@ -1,10 +1,23 @@
 <?php
 
+require 'Bonjour.php';
+
 class BonjourTest extends PHPUnit_Framework_TestCase
 
 {
-    public function testdouble(){
-$this->assertEquals(4, \Bonjour::double(2));
+    public $BonjourInstance;
+    public function setUp()
+    {
+        $this->BonjourInstance = new Bonjour();
+    }
 
+    public function testIfWheeWorks(){
+        // suppose we have 100 bonjour
+        $this->BonjourInstance->setBonjour(100);
+
+        //and we turn the wheel once
+        $this->BonjourInstance->turnwheel();
+
+        $this->assertEquals(99, $this->BonjourInstance->getBonjour()):
     }
 }

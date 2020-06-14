183b608d062e12e0b74f233ecf6a76f34383bff1
julienvinas <vinas.julien@gmail.com>
2020-06-14 21:23:35 +0200

    1

---
+++ GumballMachine.php
@@ -0,0 +1,21 @@
+<?php
+
+class GumballMachine{
+    private $gumballs;
+
+    //get the amount of gumballsr still in the machine
+    public function getGumballs(){
+        return $this->gumballs;
+    }
+
+    //set the amount of gumballs in the machine
+    public function setGumballs($amount){
+        $this->gumballs = $amount;
+    }
+
+    // the user turns the wheel, machine dispenses gumball!
+    public function turnWheel(){
+        $this->setGumballs($this->getGumballs() -1);
+    }
+
+}
---
+++ tests/GumballsMachineTest.php
@@ -0,0 +1,24 @@
+<?php
+
+require "GumballMachine.php";
+
+class GumballMachineTest extends PHPUnit_Framework_TestCase
+
+{
+    public $gumballMachineInstance;
+    public function setUp()
+    {
+        $this->gumballMachineInstance = new GumballMachine();
+    }
+
+    public function testIfWheelWorks(){
+        // suppose we have 100 gumballs...
+        $this->gumballMachineInstance->setGumballs(100);
+
+        //and we turn the wheel once
+        $this->gumballMachineInstance->turnwheel();
+
+        //we should now have 99 gumballs remaining in the machine right?
+        $this->assertEquals(99, $this->gumballMachineInstance->getGumballs());
+    }
+}
